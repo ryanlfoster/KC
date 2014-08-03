75b98b1b78f4030c0a7c072123061880b1ed4a58
Rob Stanley <r.stanley@ctidigital.com>
2014-08-03 20:10:07 +0100

    adding images and fixing getImage

--- htdocs/app/code/local/Rjs/StyleIcons/Model/Entry.php
+++ htdocs/app/code/local/Rjs/StyleIcons/Model/Entry.php
@@ -4,4 +4,9 @@ class Rjs_StyleIcons_Model_Entry extends Mage_Core_Model_Abstract
 	public function __construct() {
 		$this->_init('styleicons/entry');
 	}
+
+	public function getImage() {
+		$image = explode('/',$this->image);
+		return 'media/'.end($image);
+	}
 }
